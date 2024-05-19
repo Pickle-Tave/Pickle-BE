1d7aa8d7b291d4751b493c6ddd3ee0918b4ac72d
현태 최 <ht3064@naver.com>
2024-05-19 23:43:00 +0900

    feat/1-project-setting

---
+++ src/main/java/com/api/pickle/domain/member/domain/Member.java
@@ -0,0 +1,22 @@
+package com.api.pickle.domain.member.domain;
+
+import com.api.pickle.domain.common.model.BaseTimeEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+}
