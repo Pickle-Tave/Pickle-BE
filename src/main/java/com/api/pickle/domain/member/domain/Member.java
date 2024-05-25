a53adb6929e08e334fa125f8ea285894eeec8dd9
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-05-26 00:50:48 +0900

    feat: swagger 설정 (#17)
    * feat: member entity import 추가
    
    * feat: swagger 의존성 추가
    
    * feat: swagger config 설정 추가

--- src/main/java/com/api/pickle/domain/member/domain/Member.java
+++ src/main/java/com/api/pickle/domain/member/domain/Member.java
@@ -1,10 +1,7 @@
 package com.api.pickle.domain.member.domain;
 
 import com.api.pickle.domain.common.model.BaseTimeEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
