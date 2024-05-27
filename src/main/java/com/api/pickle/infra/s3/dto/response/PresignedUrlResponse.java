c681dac8ce41a083da0acea51550010633a8b90e
현태 최 <ht3064@naver.com>
2024-05-27 12:34:30 +0900

    feat: response dto 스키마 추가

--- src/main/java/com/api/pickle/infra/s3/dto/response/PresignedUrlResponse.java
+++ src/main/java/com/api/pickle/infra/s3/dto/response/PresignedUrlResponse.java
@@ -1,8 +1,10 @@
 package com.api.pickle.infra.s3.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
+@Schema(description = "AWS S3 응답 URL")
 @Data
 @Builder
 public class PresignedUrlResponse {
