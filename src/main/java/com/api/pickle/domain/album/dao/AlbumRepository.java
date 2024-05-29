0bc60e19a172274d84a4a2be93dffd41a24d3a25
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-05-29 17:38:46 +0900

    fix: 이미지 업로드 수정 (#31)
    * fix: infra/s3 패키지 삭제
    
    * feat: s3 properties 추가
    
    * feat: s3 config 추가
    
    * feat: 이미지 repository 추가
    
    * feat: presigned url 응답 dto 생성
    
    * feat: presigned url 요청 dto 생성
    
    * feat: 이미지 업로드 기능 구현

---
+++ src/main/java/com/api/pickle/domain/album/dao/AlbumRepository.java
@@ -0,0 +1,7 @@
+package com.api.pickle.domain.album.dao;
+
+import com.api.pickle.domain.album.domain.Album;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlbumRepository extends JpaRepository<Album, Long> {
+}
--- src/main/java/com/api/pickle/global/error/exception/ErrorCode.java
+++ src/main/java/com/api/pickle/global/error/exception/ErrorCode.java
@@ -18,6 +18,8 @@ public enum ErrorCode {
 
 
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 회원을 찾을 수 없습니다."),
+
+    ALBUM_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 앨범을 찾을 수 없습니다"),
     ;
 
     private final HttpStatus status;
