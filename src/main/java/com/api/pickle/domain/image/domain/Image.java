2a4b2b1977c6bed48a3c68cffe4de90b103efe45
현태 최 <ht3064@naver.com>
2024-05-29 17:35:14 +0900

    feat: 이미지 업로드 기능 구현

--- src/main/java/com/api/pickle/domain/image/domain/Image.java
+++ src/main/java/com/api/pickle/domain/image/domain/Image.java
@@ -1,9 +1,11 @@
 package com.api.pickle.domain.image.domain;
 
+import com.api.pickle.domain.album.domain.Album;
 import com.api.pickle.domain.common.model.BaseTimeEntity;
 import com.api.pickle.domain.member.domain.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -22,4 +24,25 @@ public class Image extends BaseTimeEntity {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "album_id")
+    private Album album;
+
+    private String imageKey;
+
+    @Builder
+    private Image(Member member, Album album, String imageKey) {
+        this.member = member;
+        this.album = album;
+        this.imageKey = imageKey;
+    }
+
+    public static Image createImage(Member member, Album album, String imageKey) {
+        return Image.builder()
+                .member(member)
+                .album(album)
+                .imageKey(imageKey)
+                .build();
+    }
 }
--- src/main/java/com/api/pickle/infra/config/s3/S3Properties.java
+++ src/main/java/com/api/pickle/infra/config/s3/S3Properties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "cloud.aws")
 @Getter
+@Setter
 public class S3Properties {
 
     private String accessKey;
