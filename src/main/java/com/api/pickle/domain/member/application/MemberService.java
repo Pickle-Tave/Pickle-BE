12ebdc1d165d6d1626a2409548d5f2000da34f4c
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-06-16 21:28:11 +0900

    feat : 로그아웃 로직 구현 (#40)

--- src/main/java/com/api/pickle/domain/member/application/MemberService.java
+++ src/main/java/com/api/pickle/domain/member/application/MemberService.java
@@ -1,6 +1,9 @@
 package com.api.pickle.domain.member.application;
 
+import com.api.pickle.domain.auth.dao.RefreshTokenRepository;
 import com.api.pickle.domain.member.dao.MemberRepository;
+import com.api.pickle.domain.member.domain.Member;
+import com.api.pickle.global.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +14,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final MemberUtil memberUtil;
+
+    public void memberLogout(){
+        final Member currentMember = memberUtil.getCurrentMember();
+        refreshTokenRepository.findById(currentMember.getId())
+                .ifPresent(refreshTokenRepository::delete);
+    }
 }
