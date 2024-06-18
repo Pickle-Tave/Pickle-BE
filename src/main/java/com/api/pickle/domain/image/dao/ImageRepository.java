b5d4fceaa32ee057ddefa4624c98becc9c141a0f
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-06-19 00:48:23 +0900

    Revert "배포 테스트 (#44)" (#51)
    This reverts commit f36bfd069da8dd17faba9eb8a4895cc4ae141665.

--- src/main/java/com/api/pickle/domain/image/dao/ImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.pickle.domain.image.dao;
-
-import com.api.pickle.domain.image.domain.Image;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ImageRepository extends JpaRepository<Image, Long> {
-}
