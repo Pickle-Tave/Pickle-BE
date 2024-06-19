ea5520b5a4604c5257817cec2fda1c8ba4bd2c27
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-06-20 02:51:06 +0900

    Revert "배포 테스트 2차 (#52)" (#57)
    This reverts commit 3d147be16f6749e099a1526c0144357cb7a6c840.

--- src/main/java/com/api/pickle/domain/album/dao/AlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.api.pickle.domain.album.dao;
-
-import com.api.pickle.domain.album.domain.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlbumRepository extends JpaRepository<Album, Long> {
-}
