5adbfb8e9234d34253e61e2f5d201fb13877179f
DESKTOP-24E7HA8\jerry <jerrymin1221@naver.com>
2024-06-26 02:58:29 +0900

    feat: 해시태그 삭제 로직 구현

--- src/main/java/com/api/pickle/domain/tag/dao/TagRepository.java
+++ src/main/java/com/api/pickle/domain/tag/dao/TagRepository.java
@@ -3,5 +3,7 @@ package com.api.pickle.domain.tag.dao;
 import com.api.pickle.domain.tag.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface TagRepository extends JpaRepository<Tag,Long> {
+    void deleteById(long tagId);
 }
