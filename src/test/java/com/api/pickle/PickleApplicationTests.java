ae60a24a3a60d6cc2cce171710c5651909c72ba7
현태 최 <ht3064@naver.com>
2024-06-27 20:04:50 +0900

    fix: redis를 모의 객체로 대체

--- src/test/java/com/api/pickle/PickleApplicationTests.java
+++ src/test/java/com/api/pickle/PickleApplicationTests.java
@@ -1,11 +1,16 @@
 package com.api.pickle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class PickleApplicationTests {
+
+	@MockBean
+	private RedisTemplate<String, Object> redisTemplate;
 
 	@Test
 	void contextLoads() {
