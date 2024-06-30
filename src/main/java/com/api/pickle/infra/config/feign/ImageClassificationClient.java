11ca255948654433e9135bdec93f12793c0a5121
현태 최 <ht3064@naver.com>
2024-06-30 23:45:33 +0900

    fix: fastapi 서버 민감한 정보 .env 파일에 관리

--- src/main/java/com/api/pickle/infra/config/feign/ImageClassificationClient.java
+++ src/main/java/com/api/pickle/infra/config/feign/ImageClassificationClient.java
@@ -2,7 +2,6 @@ package com.api.pickle.infra.config.feign;
 
 import com.api.pickle.domain.image.dto.request.ImageClassificationRequest;
 import com.api.pickle.domain.image.dto.response.ClassifiedImageResponse;
-import com.api.pickle.global.common.constants.FastAPIConstants;
 import com.api.pickle.global.config.feign.FeignConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(
         name = "imageClassificationClient",
-        url = FastAPIConstants.FAST_API_SERVER_URL,
+        url = "${fastapi.serverUrl}",
         configuration = FeignConfig.class)
 public interface ImageClassificationClient {
-    @PostMapping(value = FastAPIConstants.FAST_API_SERVER_ENDPOINT)
+    @PostMapping(value = "${fastapi.endpoint}")
     ClassifiedImageResponse getClassifiedImages(@RequestBody ImageClassificationRequest request);
 }
