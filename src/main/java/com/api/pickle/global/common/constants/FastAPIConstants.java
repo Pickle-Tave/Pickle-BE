d2f815887e6c9be589420bc52f1597a70e4e999d
현태 최 <ht3064@naver.com>
2024-06-30 22:19:32 +0900

    feat: FastAPI 서버 URL 및 엔드포인트 상수 정의

---
+++ src/main/java/com/api/pickle/global/common/constants/FastAPIConstants.java
@@ -0,0 +1,6 @@
+package com.api.pickle.global.common.constants;
+
+public final class FastAPIConstants {
+    public static final String FAST_API_SERVER_URL = "http://13.125.215.77:8000";
+    public static final String FAST_API_SERVER_ENDPOINT = "/images/classify";
+}
