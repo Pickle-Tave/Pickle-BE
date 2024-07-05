0a6d75a5aa7108634b017636ab58b0c566c3bd51
ht3064 <145987233+ht3064@users.noreply.github.com>
2024-07-05 18:04:59 +0900

    feat: FCM 기능 추가 (#123)
    * chore: fcm 설정
    
    * feat: FCM Info 추가 및 토큰 갱신, 토글 여부 변경 API 추가

---
+++ src/main/java/com/api/pickle/domain/member/dto/request/UpdateFcmTokenRequest.java
@@ -0,0 +1,13 @@
+package com.api.pickle.domain.member.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UpdateFcmTokenRequest {
+
+    @Schema(description = "FCM 토큰")
+    private String fcmToken;
+}
