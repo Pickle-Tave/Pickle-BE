3a9b5dd495e73de6654d196c5e912d1077794054
Jisun You <154600308+iiqcov@users.noreply.github.com>
2024-07-14 21:36:29 +0900

    fix: 만료된 엑세스 토큰에 대한 응답 통일 (#140)
    * fix : 만료된 엑세스 토큰에 대한 응답 통일
    
    * fix : 응답 에러를 UNAUTHORIZED로 변경

--- src/main/java/com/api/pickle/domain/auth/application/JwtTokenService.java
+++ src/main/java/com/api/pickle/domain/auth/application/JwtTokenService.java
@@ -9,7 +9,6 @@ import com.api.pickle.domain.member.domain.MemberRole;
 import com.api.pickle.global.error.exception.CustomException;
 import com.api.pickle.global.error.exception.ErrorCode;
 import com.api.pickle.global.util.JwtUtil;
-import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -53,11 +52,9 @@ public class JwtTokenService {
         SecurityContextHolder.getContext().setAuthentication(token);
     }
 
-    public AccessTokenDto retrieveAccessToken(String accessTokenValue) throws ExpiredJwtException{
+    public AccessTokenDto retrieveAccessToken(String accessTokenValue) {
         try {
             return jwtUtil.parseAccessToken(accessTokenValue);
-        } catch (ExpiredJwtException e) {
-            throw e;
         } catch (Exception e) {
             return null;
         }
