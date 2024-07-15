855c5e3485fafecc5b2ebc483274c837d8a275a1
iiqcov <iiqcov@gmail.com>
2024-07-16 00:40:30 +0900

    chore : @EnableJpaAuditing 분리

--- src/main/java/com/api/pickle/PickleApplication.java
+++ src/main/java/com/api/pickle/PickleApplication.java
@@ -2,12 +2,10 @@ package com.api.pickle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
-@EnableJpaAuditing
 public class PickleApplication {
 
 	public static void main(String[] args) {
